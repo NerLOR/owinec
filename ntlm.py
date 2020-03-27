0570c43813b8e22e70c265e3078bdcad2522bc20
Lorenz Stechauner <lorenz.stechauner@gmail.com>
2020-03-27 19:04:14 +0100

    Added NegotiateFlags in ntlm.py

--- ntlm.py
+++ ntlm.py
@@ -4,6 +4,9 @@
 # NT LAN Manager (NTLM) Authentication Protocol (NLMP) implementation
 # Lorenz Stechauner, 2020
 
+import datetime
+import struct
+
 NEGOTIATE_MESSAGE = 0x1
 CHALLENGE_MESSAGE = 0x2
 AUTHENTICATE_MESSAGE = 0x3
@@ -46,4 +49,70 @@ MsvAvChannelBindings = 0x000A
 MsvAvFlags_ACCOUNT_AUTH_CONSTRAINT = 0x00000001
 MsvAvFlags_MIC_PROVIDED = 0x00000002
 MsvAvFlags_SPN_UNTRUSTED = 0x00000004
+
+
+def _unpack_filetime(data: bytes) -> datetime.datetime:
+    return datetime.datetime(1601, 1, 1) + datetime.timedelta(microseconds=struct.unpack('<Q', data)[0] / 10)
+
+
+def _pack_filetime(timestamp: datetime.datetime) -> bytes:
+    return struct.pack('<Q', int((timestamp - datetime.datetime(1601, 1, 1)).total_seconds() * 10_000_000))
+
+
+class NegotiateFlags:
+    """
+    >>> NegotiateFlags()
+    <NegotiateFlags {}>
+    >>> NegotiateFlags(0xE20882B7)
+    <NegotiateFlags {NTLMSSP_NEGOTIATE_56, NTLMSSP_NEGOTIATE_KEY_EXCH, NTLMSSP_NEGOTIATE_128, NTLMSSP_NEGOTIATE_VERSION, NTLMSSP_NEGOTIATE_EXTENDED_SESSIONSECURITY, NTLMSSP_NEGOTIATE_ALWAYS_SIGN, NTLMSSP_NEGOTIATE_NTLM, NTLMSSP_NEGOTIATE_LM_KEY, NTLMSSP_NEGOTIATE_SEAL, NTLMSSP_NEGOTIATE_SIGN, NTLMSSP_REQUEST_TARGET, NTLM_NEGOTIATE_OEM, NTLMSSP_NEGOTIATE_UNICODE}>
+    >>> NegotiateFlags(0xE28A8235)
+    <NegotiateFlags {NTLMSSP_NEGOTIATE_56, NTLMSSP_NEGOTIATE_KEY_EXCH, NTLMSSP_NEGOTIATE_128, NTLMSSP_NEGOTIATE_VERSION, NTLMSSP_NEGOTIATE_TARGET_INFO, NTLMSSP_NEGOTIATE_EXTENDED_SESSIONSECURITY, NTLMSSP_TARGET_TYPE_SERVER, NTLMSSP_NEGOTIATE_ALWAYS_SIGN, NTLMSSP_NEGOTIATE_NTLM, NTLMSSP_NEGOTIATE_SEAL, NTLMSSP_NEGOTIATE_SIGN, NTLMSSP_REQUEST_TARGET, NTLMSSP_NEGOTIATE_UNICODE}>
+    >>> flags = NegotiateFlags(0x1)
+    >>> flags[NTLMSSP_NEGOTIATE_UNICODE]
+    True
+    >>> flags[NTLMSSP_NEGOTIATE_UNICODE] = False
+    >>> flags[NTLMSSP_NEGOTIATE_UNICODE]
+    False
+    >>> flags[NTLMSSP_NEGOTIATE_VERSION] = True
+    >>> flags[NTLMSSP_NEGOTIATE_VERSION]
+    True
+    >>> NTLMSSP_NEGOTIATE_UNICODE in flags
+    False
+    >>> NTLMSSP_NEGOTIATE_VERSION in flags
+    True
+    >>> int(NegotiateFlags())
+    0
+    >>> int(NegotiateFlags(0xE20882B7))
+    3792208567
+    >>> int(NegotiateFlags(0xE28A8235))
+    3800728117
+    """
+
+    def __init__(self, negotiate_flags: int = 0):
+        self._flags = negotiate_flags
+
+    def __setitem__(self, key: int, value: bool):
+        if value:
+            self._flags |= key
+        else:
+            self._flags &= ~key
+
+    def __getitem__(self, item: int) -> bool:
+        return self._flags & item != 0
+
+    def __contains__(self, item: int) -> bool:
+        return self[item]
+
+    def __int__(self) -> int:
+        return self._flags
+
+    def __repr__(self) -> str:
+        flags_str = []
+        for name, value in globals().items():
+            if (name.startswith('NTLM_') or name.startswith('NTLMSSP_')) and value in self:
+                flags_str.append(name)
+        return f'<NegotiateFlags {{{", ".join(flags_str)}}}>'
+
+    def __str__(self) -> str:
+        return self.__repr__()
 
