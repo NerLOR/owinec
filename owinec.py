b93b4d2e11aa18f5e07994fa389b64176ce93f07
Lorenz Stechauner <lorenz.stechauner@gmail.com>
2020-03-27 17:56:36 +0100

    Added logging and multithreading

--- owinec.py
+++ owinec.py
@@ -4,9 +4,13 @@
 # Lorenz Stechauner, 2020
 
 import argparse
+from http import HTTPStatus
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import ssl
 import ipaddress
+import logging
+from socketserver import ThreadingMixIn
+import threading
 
 WSMAN_PORT_HTTP = 5985
 WSMAN_PORT_HTTPS = 5986
@@ -15,10 +19,30 @@ WSMAN_PORT_HTTPS = 5986
 class WSManHandler(BaseHTTPRequestHandler):
     server_version = 'owinec/1.0'
 
+    def do_GET(self):
+        self.send_response(HTTPStatus.METHOD_NOT_ALLOWED)
+        self.wfile.write(b'Method Not Allowed')
+
+    def do_PUT(self):
+        self.send_response(HTTPStatus.METHOD_NOT_ALLOWED)
+        self.wfile.write(b'Method Not Allowed')
+
     def do_POST(self):
-        self.send_response(501)
+        threading.current_thread().setName(self.client_address[0])
+        logger.debug(f'Got request from {self.client_address[0]}')
+        self.send_response(HTTPStatus.NOT_IMPLEMENTED)
         self.end_headers()
         self.wfile.write(b'Not Implemented')
+
+    def send_response(self, code: HTTPStatus, message=None):
+        super().send_response(code, message=message)
+
+    def log_message(self, format, *args):
+        return
+
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    pass
 
 
 if __name__ == '__main__':
@@ -33,18 +57,39 @@ if __name__ == '__main__':
                         help='The certificate file to use for https')
     parser.add_argument('--key', type=argparse.FileType('r'),
                         help='The private key file to use for https')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='Be verbose')
     args = parser.parse_args()
 
+    logger = logging.getLogger('owinec')
+    logger.setLevel(logging.DEBUG)
+
+    cmd_handler = logging.StreamHandler()
+    cmd_handler.setFormatter(logging.Formatter('[%(levelname)s][%(threadName)s] %(message)s'))
+    logger.addHandler(cmd_handler)
+
+    if args.verbose:
+        cmd_handler.setLevel(logging.DEBUG)
+    else:
+        cmd_handler.setLevel(logging.INFO)
+
+    logger.debug('Starting owinec...')
+    logger.debug(f'Command line arguments: {args}')
+
     if args.protocol in ('http', 'https'):
+        logger.debug(f'Using protocol {args.protocol}')
         bind_address = str(args.listen_address)
         bind_port = args.port or WSMAN_PORT_HTTP if args.protocol == 'http' else WSMAN_PORT_HTTPS
-        httpd = HTTPServer((bind_address, bind_port), WSManHandler)
+
+        httpd = ThreadedHTTPServer((bind_address, bind_port), WSManHandler)
 
         if args.protocol == 'https':
             if not args.cert or not args.key:
                 raise FileNotFoundError('certificate and private key have to be specified when using https')
             httpd.socket = ssl.wrap_socket(httpd.socket, server_side=True,
                                            certfile=args.cert.name, keyfile=args.key.name)
+
+        logger.info(f'Listening on {args.protocol}://{bind_address}:{bind_port}/')
 
         httpd.serve_forever()
     else:
